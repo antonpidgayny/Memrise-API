521b5eef74247877370fe54c7ea88956128a5628
Anton <antonpidgayny@gmail.com>
2018-04-26 03:17:43 +0300

    Update Course.js

--- build/handlers/Course.js
+++ build/handlers/Course.js
@@ -1,60 +1,70 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Slave_1 = require("./Slave");
+declare var require: any;
+import Slave from './Slave';
+import * as iconv from 'iconv-lite';
 var DomParser = require('dom-parser');
-var Course = /** @class */ (function () {
-    function Course() {
-    }
-    Course.prototype.setURL = function (url) { this.Url = url; };
-    Course.prototype.getURL = function () { return this.Url; };
-    Course.prototype.getLessonByID = function (id, callback) {
-        var url = this.Url + id + '/';
-        Slave_1.default.webPageToStr(url, function (err, html) {
-            var parser = new DomParser();
-            var dom = parser.parseFromString(html);
-            var things = dom.getElementsByClassName("thing text-text");
-            var words = [];
-            things.forEach(function (item, i, arr) {
-                words.push({
-                    word: item.getElementsByClassName("col_a col text")[0].textContent,
-                    translation: item.getElementsByClassName("col_b col text")[0].textContent
-                });
-            });
-            return callback(null, words);
-        });
-    };
-    Course.prototype.getCourse = function (callback) {
-        var _this = this;
-        var parser = new DomParser();
-        Slave_1.default.webPageToStr(this.Url, function (err, html) {
-            var dom = parser.parseFromString(html);
-            var lessons = dom.getElementsByClassName('level-index');
-            var num_lessons = lessons.length;
-            var course = [];
-            var _loop_1 = function (i) {
-                _this.getLessonByID(i, function (err, words) {
-                    course.push({ lesson_id: i, words: words });
-                    if (course.length === num_lessons) {
-                        course.sort(function (a, b) {
-                            if (a.lesson_id > b.lesson_id) {
-                                return 1;
-                            }
-                            if (a.lesson_id < b.lesson_id) {
-                                return -1;
-                            }
-                            return 0;
-                        });
-                        return callback(null, course);
-                    }
-                });
-            };
-            for (var i = 1; i <= num_lessons; i++) {
-                _loop_1(i);
-            }
-        });
-    };
-    ;
-    return Course;
-}());
-exports.default = new Course();
-//# sourceMappingURL=Course.js.map
+
+class Course {
+	
+	private name: string;
+
+	private id: string;	
+
+	constructor(Name: string, Id: string) {
+		this.name=Name;
+		this.id=Id;
+	}
+
+	public setName(Name: string): void {this.name=Name}
+
+	public getName(): string {return this.name}
+
+	public setID(Id: string): void {this.id=Id}
+
+	public getID(): string {return this.id}
+	
+	public getLessonByID(ID: number , callback): void{
+		let url = "https://www.memrise.com/course/" + this.id +'/'+ this.name +'/'+ ID +'/';
+		Slave.webPageToStr(url, (err, html) => {	
+			let parser = new DomParser();	
+			let dom = parser.parseFromString(html);
+			let things = dom.getElementsByClassName("thing text-text");
+			let words = [];
+			things.forEach(function(item,i,arr){
+				words.push(
+				{	
+					word: item.getElementsByClassName("col_a col text")[0].textContent,
+					translation: item.getElementsByClassName("col_b col text")[0].textContent
+				})
+			});
+			return callback(null, words);
+		})
+	}
+
+	public getCourse(callback): void{
+		let parser = new DomParser();	
+		Slave.webPageToStr("https://www.memrise.com/course/" + this.id +'/'+ this.name +'/', (err, html) => {
+			let dom = parser.parseFromString(html);	
+			let lessons = dom.getElementsByClassName('level-index');
+			let num_lessons = lessons.length;
+			let course = [];
+			for (let i = 1; i <= num_lessons; i++) {
+				this.getLessonByID(i , (err, words) => {
+					course.push({lesson_id: i, words: words});
+					if (course.length === num_lessons) {
+						course.sort((a,b) => {
+							if (a.lesson_id > b.lesson_id) {return 1}
+							if (a.lesson_id < b.lesson_id) {return -1}
+							return 0;	
+						});
+						return callback(null, course);
+					}
+				})
+			}
+		});
+	};
+}
+
+export default new Course("","");
+
+
+ 
